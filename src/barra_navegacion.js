1d12de0d2ed7b77c70e32924ddf2c686e82291b9
Rolando Ordoñez <roloordonez2001@hotmail.com>
2024-05-17 15:38:26 -0600

    agregado
    agregue lo que he avanzado

---
+++ src/barra_navegacion.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './src/barra_navegacion.css';
+
+const NavBar = () => (
+  <div className="navbar">
+    <img src="/Color-YouTube-logo.png" alt="YouTube" className="logo" />
+    <input type="text" placeholder="Buscar" className="search-bar" />
+    <button className="search-button">🔍</button>
+    <div className="nav-icons">
+      <button>🎤</button>
+      <button>🔔</button>
+      <img src="profile.jpg" alt="Profile" className="profile-pic" />
+    </div>
+  </div>
+);
+
+export default NavBar;
