179f0630d5b205d694f0e08999ffc88d37b5c608
Rolando Ordoñez <roloordonez2001@hotmail.com>
2024-05-19 10:53:18 -0600

    barra_navegacion
    barra de navegacion modificada

--- src/barra_navegacion.js
+++ src/barra_navegacion.js
@@ -1,15 +1,18 @@
+// src/components/molecules/NavBar.js
 import React from 'react';
 import './barra_navegacion.css';
 
 const NavBar = () => (
   <div className="navbar">
-    <img src="/Color-YouTube-logo.png" alt="YouTube" className="logo" />
-    <input type="text" placeholder="Buscar" className="search-bar" />
-    <button className="search-button">🔍</button>
+    <img src="/Color-Youtube-logo.png" alt="Logo" className="logo" />  {/* Imagen en la barra de navegación */}
+    <div className="search-container">
+      <input type="text" placeholder="Buscar" className="search-bar" readOnly/>
+      <button className="search-button">Buscar</button>
+    </div>
     <div className="nav-icons">
-      <button>🎤</button>
-      <button>🔔</button>
-      <img src="profile.jpg" alt="Profile" className="profile-pic" />
+      <button>Icono1</button>
+      <button>Icono2</button>
+      <img src="/profile.jpg" alt="Profile" className="profile-pic" />
     </div>
   </div>
 );
